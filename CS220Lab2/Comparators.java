3ffe5ca626cc6defdd62f33450aea43873b66c56
Aiden Montgomery <Aidenmontg@icloud.com>
2025-09-29 23:11:21 -0400

    javadoc, apparently i commited to the wrong spot

--- CS220Lab2/Comparators.java
+++ CS220Lab2/Comparators.java
@@ -2,9 +2,13 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-// for reference https://www.geeksforgeeks.org/java/java-comparator-interface/
+// [1] Geeks for Geeks. java comparator interface. https://www.geeksforgeeks.org/java/java-comparator-interface/. 31 Jul, 2025
 
-public class Project2Driver{
+
+/**
+ * Driver class
+ */
+public class Comparators{
     public static void main(String[] args) {
         List<Employee> employees = new ArrayList<>();
 
@@ -24,21 +28,51 @@ public class Project2Driver{
     }
 }
 
+/**
+ * The class representing Employees
+ * Contains name, age, and salary and a "to string" method
+ */
 class Employee{
     String name;
     Integer age;
     Integer salary;
 
+    /**
+     * Just a constructor method for the class
+     *
+     * @param name Employee name
+     * @param age Employee Age
+     * @param salary Employee Salary
+     */
     Employee(String name, int age, int salary){
         this.name = name;
         this.age = age;
         this.salary = salary;
     }
 
+    /**
+     * Gives a string representation of the object
+     *
+     * @return a String representation of the object
+     */
     String getInfo(){return this.name + ": " + this.age + ", " + this.salary;}
 }
 
+
+/**
+ * Class for sorting through Employees
+ */
 class EmployeeSorter implements Comparator<Employee>{
+
+    /**
+     * Compares two employees and moves them accordingly for
+     * age in ascending order, then by salary in descending order,
+     * then by name in alphabetical order.
+     *
+     * @param e1 Employee 1
+     * @param e2 Employee 2
+     * @return int of -1, 0, or 1 representing where we're moving our employees
+     */
     public int compare(Employee e1, Employee e2){
         int ageCompare = e1.age.compareTo(e2.age);
         if (ageCompare != 0){return ageCompare;}
